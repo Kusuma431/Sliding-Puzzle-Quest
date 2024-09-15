98362dac717e7f7f9a4b1b9de79bc7d785914155
Bellamkonda Kusuma Sirisha <144311437+Kusuma431@users.noreply.github.com>
2024-09-15 15:54:51 +0530

    Create UserBean.java

---
+++ UserBean.java
@@ -0,0 +1,13 @@
+package test;
+public class UserBean {
+    private String uName;
+    private String pWord;
+    private String fName;    
+    public String getuName() { return uName; }
+    public void setuName(String uName) { this.uName = uName; }
+    public String getpWord() { return pWord; }
+    public void setpWord(String pWord) { this.pWord = pWord; }
+    public String getfName() { return fName; }
+    public void setfName(String fName) { this.fName = fName; 
+}    
+}
