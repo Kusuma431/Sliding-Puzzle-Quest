8afd8cac7f0ea4420db595a4b57e9dfede87ddc9
Bellamkonda Kusuma Sirisha <144311437+Kusuma431@users.noreply.github.com>
2024-09-15 15:41:44 +0530

    Create LoginServlet.java

---
+++ LoginServlet.java
@@ -0,0 +1,32 @@
+package test;
+
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@SuppressWarnings("serial")
+@WebServlet("/log")
+public class LoginServlet extends HttpServlet {
+    private LoginDAO ld = null;
+
+    @Override
+    public void init() throws ServletException {
+        ld = new LoginDAO();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String uN = req.getParameter("uname");
+        String pW = req.getParameter("pword");
+        UserBean ub = ld.login(uN, pW);
+        if (ub != null) {
+            HttpSession session = req.getSession();
+            session.setAttribute("ubean", ub);
+            RequestDispatcher rd = req.getRequestDispatcher("LoginSuccess.jsp");
+            rd.forward(req, res);
+        } else {
+            res.sendRedirect("main.html");
+        }
+    }
+}
